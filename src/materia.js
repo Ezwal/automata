3e02bb639a374a1089e1ee109bddcb53fcb5e105
Ezwal <ezwal@github.com>
2020-12-28 12:47:01 +0100

    Add gaz materia

--- src/materia.js
+++ src/materia.js
@@ -1,30 +1,41 @@
-import { waterPhysics, gravity } from './physics.js'
+import { waterPhysics, gravity, gazPhysics } from './physics.js'
 
 const materia = Object.freeze({
     air: 0,
     ground: 1,
     water: 2,
     sand: 3,
+    gaz: 4,
 })
 
 const props = Object.freeze({
     [materia.air]: {
-        density: 0,
+        density: 10,
         color: [255, 255, 255],
+        key: 'a',
     },
     [materia.ground]: {
         density: 100,
         color: [0, 0, 0],
+        key: 't',
     },
     [materia.water]: {
         density: 40,
         color: [0, 15, 255],
-        physic: waterPhysics
+        physic: waterPhysics,
+        key: 'w',
     },
     [materia.sand]: {
         density: 50,
         color: [244, 217, 14],
-        physic: gravity
+        physic: gravity,
+        key: 's',
+    },
+    [materia.gaz]: {
+        density: 0,
+        color: [19, 199, 244],
+        physic: gazPhysics,
+        key: 'g',
     }
 })
 
--- src/physics.js
+++ src/physics.js
@@ -1,11 +1,11 @@
-import { is, at, swap, down, left, right } from './world.js'
+import { is, at, swap, up, down, left, right } from './world.js'
 import { materia, props } from './materia.js'
 import { scramble } from './util.js'
 
 function gravity(center) {
     const centerDensity = props[at(center)].density
     const belowIndex = down(center)
-    for (let toCheck of [belowIndex].concat(scramble(left(belowIndex), right(down(center))))) {
+    for (let toCheck of [belowIndex].concat(scramble(left(belowIndex), right(belowIndex)))) {
         let toCheckDensity = props[at(toCheck)]?.density
         if (toCheckDensity < centerDensity) {
             return swap(toCheck, center)
@@ -27,4 +27,16 @@ function waterPhysics(center) {
     return []
 }
 
-export { waterPhysics, gravity }
+function gazPhysics(center) {
+    const centerDensity = props[at(center)].density
+    const upperIndex = up(center)
+    for (let toCheck of [upperIndex].concat(scramble(left(upperIndex), right(upperIndex)))) {
+        let toCheckDensity = props[at(toCheck)]?.density
+        if (toCheckDensity > centerDensity) {
+            return swap(toCheck, center)
+        }
+    }
+    return []
+}
+
+export { waterPhysics, gravity, gazPhysics }
