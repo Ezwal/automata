8d95ead2cfe0514054d1a647ef0a8d076625d55d
Ezwal <ezwal@github.com>
2020-12-28 12:47:34 +0100

    more natural propagation ?

--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
 const halfChance = () => Math.random() > 0.5
 
-const scramble = (a, b) => Math.floor(a / 100) % 2 === 0 ? [a, b] : [b, a]
+const scramble = (a, b) => halfChance() ? [a, b] : [b, a]
 
 export { scramble }
